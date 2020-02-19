d4e589873ea431fd66def41a120fe9d6d0727271
RitishKarki <karki.ritish@gmail.com>
2020-02-19 14:12:52 -0500

    UserProfile class added

--- MarketPlace/Model/UserProfile.swift
+++ MarketPlace/Model/UserProfile.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 Ashish-Ritish. All rights reserved.
 //
 
-import Foundation
+class UserProfile{
+    var name: String
+    var email: String
+    var contactNumber: String
+    var zipCode: String
+    var address: String
+
+    init?(name: String, email: String, contactNumber: String, zipCode: String, address: String) {
+        if name.isEmpty || email.isEmpty || contactNumber.isEmpty{
+            return nil
+        }
+        
+        self.name = name
+        self.email = email
+        self.contactNumber = contactNumber
+        self.zipCode = String
+        self.address = address
+        
+    }
+}

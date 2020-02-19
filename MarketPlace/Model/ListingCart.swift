13a7a18b131d5a530a4ca55b9fe49b011e8a1f19
ashre1 <ashre1@unh.newhaven.edu>
2020-02-19 13:53:09 -0500

    Model class created

---
+++ MarketPlace/Model/ListingCart.swift
@@ -0,0 +1,9 @@
+//
+//  ListingCart.swift
+//  MarketPlace
+//
+//  Created by Ashish Shrestha on 2/19/20.
+//  Copyright © 2020 Ashish-Ritish. All rights reserved.
+//
+
+import Foundation
---
+++ MarketPlace/Model/ProductItem.swift
@@ -0,0 +1,6 @@
+//  File.swift
+//  MarketPlace
+//
+//  Created by Ashish Shrestha and Ritish Karki on 2/19/20.
+//  Copyright © 2020 Ashish-Ritish. All rights reserved.
+

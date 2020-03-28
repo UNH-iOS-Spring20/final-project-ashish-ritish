d20be47ca0b556110e561e3e88f09ccede2af147
ritish karki <ritish@ritishs-MacBook-Pro.local>
2020-03-28 16:12:34 -0400

    enhances and fixes to tab view in listing screen

--- MarketPlace/View/productListing/ListScreenView.swift
+++ MarketPlace/View/productListing/ListScreenView.swift
@@ -9,78 +9,78 @@
 import SwiftUI
 
 struct ListScreenView: View {
-    @State var itemColor = "favorite"
-    
+    @State var selectedTab = "favorite"
     
     var body: some View {
        NavigationView{
         GeometryReader{ geometry in
-            VStack(spacing: -4){
-                HStack(spacing: 0){
-                    
+            VStack(){
+                HStack{
                     VStack(spacing: 5){
                         Text("Favorite")
-                            .font(.system(size: 18))
-                            .foregroundColor(self.itemColor == "favorite" ? .blue : .gray)
-                        Image(systemName: "minus")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 10)
-                            .foregroundColor(self.itemColor == "favorite" ? .blue : .clear)
+                        .font(.system(size: 18))
+                        .foregroundColor(self.selectedTab == "favorite" ? .blue : .gray)
+                        GeometryReader { g in
+                            Rectangle()
+                                .fill(self.selectedTab == "favorite" ? Color.blue : Color.clear)
+                                .frame(width: g.size.width, height: 2)
+                        }
                     }
                     .onTapGesture {
-                            self.itemColor = "favorite"
+                            self.selectedTab = "favorite"
                     }
                     
                     VStack(spacing: 5){
                         Text("Selling")
                             .font(.system(size: 18))
-                            .foregroundColor(self.itemColor == "selling" ? .blue : .gray)
-                        Image(systemName: "minus")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 10)
-                            .foregroundColor(self.itemColor == "selling" ? .blue : .clear)
+                            .foregroundColor(self.selectedTab == "selling" ? .blue : .gray)
+                        GeometryReader { g in
+                            Rectangle()
+                                .fill(self.selectedTab == "selling" ? Color.blue : Color.clear)
+                                .frame(width: g.size.width, height: 2)
+                        }
                     }
                     .onTapGesture {
-                            self.itemColor = "selling"
+                            self.selectedTab = "selling"
                     }
                     
                     VStack(spacing: 5){
                         Text("Sold")
                             .font(.system(size: 18))
-                            .foregroundColor(self.itemColor == "sold" ? .blue : .gray)
-                        Image(systemName: "minus")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 10)
-                            .foregroundColor(self.itemColor == "sold" ? .blue : .clear)
+                            .foregroundColor(self.selectedTab == "sold" ? .blue : .gray)
+                        GeometryReader { g in
+                            Rectangle()
+                                .fill(self.selectedTab == "sold" ? Color.blue : Color.clear)
+                                .frame(width: g.size.width, height: 2)
+                        }
                     }
-                        
                     .onTapGesture {
-                            self.itemColor = "sold"
+                            self.selectedTab = "sold"
                     }
                     
                     VStack(spacing: 5){
-                        Text("Brought")
+                        Text("Bought")
                             .font(.system(size: 18))
-                            .foregroundColor(self.itemColor == "brought" ? .blue : .gray)
-                        Image(systemName: "minus")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 10)
-                            .foregroundColor(self.itemColor == "brought" ? .blue : .clear)
+                            .foregroundColor(self.selectedTab == "bought" ? .blue : .gray)
+                        GeometryReader { g in
+                            Rectangle()
+                                .fill(self.selectedTab == "bought" ? Color.blue : Color.clear)
+                                .frame(width: g.size.width, height: 2)
+                        }
                     }
-                        
                     .onTapGesture {
-                            self.itemColor = "brought"
+                            self.selectedTab = "bought"
                     }
-                    
-                } .frame(width: geometry.size.width, height: geometry.size.height/15)
-                    .background(Color.white)
-                    .padding(.top, 5)
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height/15)
+                .background(Color.white)
+                .padding(.top, 10)
+                VStack{
+                    HStack{
+                        
+                        Text(self.selectedTab)                    }
+                }
                 Spacer()
-                Color.red
             }
         }
        .navigationBarTitle(Text("Listing"), displayMode: .inline)

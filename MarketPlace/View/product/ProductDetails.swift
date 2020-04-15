82b62eb7e94e3e1d8b7a83df6207eb00c9ae4415
ashre1 <ashre1@unh.newhaven.edu>
2020-04-15 19:32:28 -0400

    Image slider corrected

--- MarketPlace/View/product/ProductDetails.swift
+++ MarketPlace/View/product/ProductDetails.swift
@@ -13,8 +13,8 @@ struct ProductDetails: View {
     
     func updateFavorite() {
         if !product.name.isEmpty && !product.category.isEmpty && !product.condition.isEmpty && !product.description.isEmpty &&
-        !product.email.isEmpty && !product.imageUrls.isEmpty && !product.imageName.isEmpty && !String(product.isFavorite).isEmpty && !String(product.latitude).isEmpty && !String(product.longitude).isEmpty && !String(product.price).isEmpty {
-        
+            !product.email.isEmpty && !product.imageUrls.isEmpty && !product.imageName.isEmpty && !String(product.isFavorite).isEmpty && !String(product.latitude).isEmpty && !String(product.longitude).isEmpty && !String(product.price).isEmpty {
+            
             productsCollectionRef.document(self.product.id).setData(self.product.data)
             print(self.product.data)
         }
@@ -24,26 +24,25 @@ struct ProductDetails: View {
     }
     
     var body: some View {
-        VStack{
-            ProductImageView(self.product.imageUrls.map { ProductImage(picture: $0) })//.frame(height: 250)
-                .aspectRatio(contentMode: .fit)
-            VStack (alignment: .leading){
-                HStack{
-                    
-                    Spacer()
-                    Button(action: {
-                        self.product.isFavorite.toggle()
-                        self.updateFavorite()
-                    }){
-                        if  product.isFavorite {
-                            Image(systemName: "heart.fill")
-                                .foregroundColor(Color.red)
-                        }else{
-                            Image(systemName: "heart")
-                                .foregroundColor(Color.gray)
-                        }
-                    }//.buttonStyle(BorderlessButtonStyle())
+        VStack(spacing : 0){
+            ProductImageView(self.product.imageUrls.map { ProductImage(picture: $0) })
+            HStack{
+                Spacer()
+                Button(action: {
+                    self.product.isFavorite.toggle()
+                    self.updateFavorite()
+                }){
+                    if  product.isFavorite {
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(Color.red)
+                    }else{
+                        Image(systemName: "heart")
+                            .foregroundColor(Color.gray)
+                    }
                 }
+            }.padding(20)
+            VStack (spacing: 15){
+                
                 
                 HStack{
                     Text("Name:")
@@ -53,7 +52,7 @@ struct ProductDetails: View {
                     Text(product.name)
                         .font(.headline)
                 }
-                .padding(10)
+        
                 HStack{
                     Text("Category:")
                         .font(.headline)
@@ -62,7 +61,8 @@ struct ProductDetails: View {
                     Text(product.category)
                         .font(.headline)
                     
-                }.padding(10)
+                }
+              
                 HStack{
                     Text("Condition:")
                         .font(.headline)
@@ -72,7 +72,7 @@ struct ProductDetails: View {
                         .font(.headline)
                     
                 }
-                .padding(10)
+            
                 HStack{
                     Text("Email")
                         .font(.headline)
@@ -81,7 +81,7 @@ struct ProductDetails: View {
                     Text(product.email)
                         .font(.headline)
                 }
-                .padding(10)
+            
                 HStack{
                     Text("Price:")
                         .font(.headline)
@@ -91,19 +91,19 @@ struct ProductDetails: View {
                         .font(.headline)
                         .padding(.leading,50)
                 }
-                .padding(10)
             }
             .padding()
             
             MapView(location: [product.latitude, product.longitude])
-                .frame(height: 200)
+                .frame(height: 160)
         }
         .navigationBarTitle(Text(product.name), displayMode: .inline)
-        .padding(.top, 10)
     }
 }
 
-var sampleProudct = Product(id: "1234", data: [ "name": "Iphone8", "price": 622.00, "email": "rkark1@unh.newhaven.edu", "category": "Cell Phone", "condition": "New", "imageName": "Ihone8", "latitude": 41.26201, "longitude": -72.94621, "description": "Sample phone Data", "isFavorite": true, "imageUrls": ["https://cdn.pixabay.com/photo/2016/02/13/13/11/cuba-1197800_1280.jpg","https://firebasestorage.googleapis.com/v0/b/marketplace-71120.appspot.com/o/BMW.png?alt=media&token=aca57096-47ee-46d9-86e2-f81c81cb0c14","https://cdn.pixabay.com/photo/2016/02/13/13/11/cuba-1197800_1280.jpg"]])
+var sampleProudct = Product(id: "1234", data: [ "name": "Iphone8", "price": 622.00, "email": "rkark1@unh.newhaven.edu", "category": "Cell Phone", "condition": "New", "imageName": "Ihone8", "latitude": 41.26201, "longitude": -72.94621, "description": "Sample phone Data", "isFavorite": true, "imageUrls": ["https://firebasestorage.googleapis.com/v0/b/marketplace-71120.appspot.com/o/Real%20State%2FGhar%2F00B0B_imKuvG07Bhf_600x450.jpg?alt=media&token=b8ef0d5c-dbea-4d71-a6a3-c25416792262",
+                                                                                                                                                                                                                                                                                                                 "https://firebasestorage.googleapis.com/v0/b/marketplace-71120.appspot.com/o/Real%20State%2FGhar%2F00m0m_7xa163K3Jyc_600x450.jpg?alt=media&token=2a8749ec-e810-4729-83f6-33faaebde207",
+                                                                                                                                                                                                                                                                                                                 "https://firebasestorage.googleapis.com/v0/b/marketplace-71120.appspot.com/o/Real%20State%2FGhar%2F00P0P_57FBcuR9IdY_600x450.jpg?alt=media&token=66afdb55-51f2-4ca7-9053-b906afab8f16"]])
 
 struct ProductDetails_Previews: PreviewProvider {
     static var previews: some View {
